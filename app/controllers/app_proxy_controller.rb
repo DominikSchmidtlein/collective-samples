df3bc43d3fcfb69e4a53dd66b26a540c73604879
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-05-18 10:36:35 -0400

    display supplier's products on vendor's store

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -2,6 +2,17 @@ class AppProxyController < ApplicationController
    include ShopifyApp::AppProxyVerification
 
   def index
+    @shops = Shop.all
+    shop = Shop.find_by(shopify_domain: params[:shop])
+    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
+    ShopifyAPI::Base.activate_session(shop)
+    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+
+    shop = Shop.find_by(shopify_domain: 'sharetest052018.myshopify.com')
+    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
+    ShopifyAPI::Base.activate_session(shop)
+    @products += ShopifyAPI::Product.find(:all, params: { limit: 10 })
+
     render layout: false, content_type: 'application/liquid'
   end
 
