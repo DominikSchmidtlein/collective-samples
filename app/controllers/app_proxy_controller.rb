c102a175f292d972dd119ea520a12eb7430f87c1
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-05-20 23:27:45 -0400

    display samples in app proxy based on database model

--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -1,17 +1,20 @@
 class AppProxyController < ApplicationController
-   include ShopifyApp::AppProxyVerification
+  include ShopifyApp::AppProxyVerification
 
   def index
     @shops = Shop.all
-    shop = Shop.find_by(shopify_domain: params[:shop])
-    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
-    ShopifyAPI::Base.activate_session(shop)
-    @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    @products = []
 
-    shop = Shop.find_by(shopify_domain: 'sharetest052018.myshopify.com')
-    shop = ShopifyApp::SessionRepository.retrieve(shop.id)
-    ShopifyAPI::Base.activate_session(shop)
-    @products += ShopifyAPI::Product.find(:all, params: { limit: 10 })
+    shop = Shop.find_by(shopify_domain: params[:shop])
+    shop.supplier_partnerships.each do |partnership|
+      supplier_shop_id = partnership.supplier_shop_id
+      supplier_session = ShopifyApp::SessionRepository.retrieve(supplier_shop_id)
+      ShopifyAPI::Base.activate_session(supplier_session)
+
+      @products += partnership.samples.map do |sample|
+        ShopifyAPI::Product.find(sample.shopify_product_id)
+      end
+    end
 
     render layout: false, content_type: 'application/liquid'
   end
