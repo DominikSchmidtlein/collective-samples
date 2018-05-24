f1a9678ae7a9e389e6682c9ec4225e003c03b902
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-05-23 22:57:00 -0400

    enable dependent destroy for shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,7 @@
 class Shop < ActiveRecord::Base
   include ShopifyApp::SessionStorage
 
-  has_many :sample_orders
-  has_many :supplier_partnerships, class_name: 'Partnership', foreign_key: 'vendor_id'
-  has_many :vendor_partnerships, class_name: 'Partnership', foreign_key: 'supplier_id'
+  has_many :sample_orders, dependent: :destroy
+  has_many :supplier_partnerships, class_name: 'Partnership', foreign_key: 'vendor_id', dependent: :destroy
+  has_many :vendor_partnerships, class_name: 'Partnership', foreign_key: 'supplier_id', dependent: :destroy
 end
